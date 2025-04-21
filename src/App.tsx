f08e1836d5119f02b3426263963d606a9eb850cf
Gokul <gokulthangavell@gmail.com>
2025-04-22 00:56:00 +0530

    Added Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-// import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button colorScheme={"blue"}>Click me</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
