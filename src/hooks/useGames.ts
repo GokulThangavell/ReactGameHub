8b63922530210e29c6aa0e8c000ea983ef7bf2a1
Gokul <gokulthangavell@gmail.com>
2025-04-26 10:12:52 +0530

    Added platforms

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,14 +1,6 @@
-import apiClient from "@/services/api.client";
-import { CanceledError } from "axios";
-import { useEffect, useState } from "react";
 import useData from "./useData";
 import { Genre } from "./useGenres";
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { Platform } from "./usePlatform";
 
 export interface Game {
   id: number;
