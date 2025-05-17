a406be4d6c5de7746b2a183751aab72bb812a362
Gokul <gokulthangavell@gmail.com>
2025-05-17 20:45:44 +0530

    Added infinite scrolling

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,40 +1,76 @@
-import useGames from "@/hooks/useGames";
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames, { Game } from "@/hooks/useGames";
+import { Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "@/App";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data: games, error, isLoading } = useGames(gameQuery);
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   if (error) return <Text>{error.message}</Text>;
 
+  const fetchedGamesCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      padding="10px"
-      spacing={6}
-    >
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer key={skeleton}>
-            {" "}
-            <GameCardSkeleton></GameCardSkeleton>
-          </GameCardContainer>
+    <>
+      <InfiniteScroll
+        dataLength={fetchedGamesCount}
+        hasMore={!!hasNextPage}
+        next={() => fetchNextPage()}
+        loader={<Spinner />}
+      ></InfiniteScroll>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={6}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              {" "}
+              <GameCardSkeleton></GameCardSkeleton>
+            </GameCardContainer>
+          ))}
+
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page?.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game}></GameCard>
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
         ))}
-      {games?.results.map((game) => (
+
+        {/* {data?.results.map((game, index) => (
         <GameCardContainer key={game.id}>
-          <GameCard key={game.id} game={game}></GameCard>
+          <GameCard game={game}></GameCard>
         </GameCardContainer>
-      ))}
-    </SimpleGrid>
+      ))} */}
+      </SimpleGrid>
+      {/* {hasNextPage && (
+        <Button onClick={() => fetchNextPage()}>
+          {isFetchingNextPage ? "Loading.." : "Load more"}
+        </Button>
+      )} */}
+    </>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
 import { GameQuery } from "@/App";
 import { Platform } from "./usePlatform";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import APIClient from "@/services/api.client";
+import APIClient, { FetchResponse } from "@/services/api.client";
 
 export interface Game {
   id: number;
@@ -14,8 +14,27 @@ export interface Game {
 
 const apiClient = new APIClient<Game>("/games");
 
-const useGames = (gameQuery: GameQuery) =>
-  useQuery({
+const fetchData = async ({ pageParam = 0 }) => {
+  return await fetch(`.../?page=${pageParam}`).then((res) => res.json());
+};
+
+// const useInfiniteGames = (gameQuery: GameQuery) => {
+//   useInfiniteQuery<FetchResponse<Game>, Error>({
+//     queryKey: ["infiniteScroll", gameQuery],
+//     queryFn: () =>
+//       apiClient.getAll({
+//         params: {
+//           genres: gameQuery.genreId,
+//           parent_platforms: gameQuery.platformId,
+//           ordering: gameQuery.sortOrder,
+//           search: gameQuery.searchText,
+//         },
+//       }),
+//   });
+// };
+
+const useGames = (gameQuery: GameQuery) => {
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: () =>
       apiClient.getAll({
@@ -26,7 +45,12 @@ const useGames = (gameQuery: GameQuery) =>
           search: gameQuery.searchText,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    staleTime: 24 * 60 * 60 * 1000, //24 hours
   });
+};
 
 // useData<Game>(
 //   "/games",
